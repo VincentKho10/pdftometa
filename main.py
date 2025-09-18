cea4eee74cf839e97526da0918b51fa964f68650
pembe <pembe@ADMIN8PEMBELIAN>
2025-09-18 15:49:11 +0700

    Released

--- main.py
+++ main.py
@@ -1,28 +1,54 @@
+import os
 import pymupdf
 import re
+import locale
 
+locale.setlocale(locale.LC_ALL, 'en_ID.UTF-8')
+
+res={}
 def main():
-    doc = pymupdf.open("build/input/test.pdf")
-    out = open("output.pdf", "wb")
-    pattern = r"ORDER NO\n:\n(.*)|GRAND TOTAL\n:\nIDR\n(.*)\n|^[0-9]$\n(.*\n.*\n.*\n.*\n.*)"
-    dict = {}
-    for page in doc:
-        text = page.get_text()
-        matches = re.finditer(pattern, text, re.MULTILINE)
-        for matchNum, match in enumerate(matches, start=1):
-        # ponum = [v for v in matched[0] if v!=""][0]
-        # grand_total = [v for v in matched[1] if v!=""][0]
-        # items = [v for v in matched[2] if v!=""][0]
-        # dict["ponum"] = ponum
-        # dict["grand_total"] = grand_total
-        # dict["items"] = items
-            #  print ("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
-            print(type(match.group()))
-            for groupNum in range(0, len(match.groups())):
-                groupNum = groupNum + 1
-                # print(match.group(groupNum))
-                # print ("Group {groupNum} found at {start}-{end}: {group}".format(groupNum = groupNum, start = match.start(groupNum), end = match.end(groupNum), group = match.group(groupNum)))
-    out.close()
+    pattern = r"ORDER NO\n:\n(.*)|GRAND TOTAL\n:\nIDR\n(.*)\n"
+    lsof = [ "input/{f}".format(f=v) for v in os.listdir("input")]
+    for v in lsof:
+        if (v.endswith('.pdf')):
+            doc = pymupdf.open(v)
+            convert(doc, pattern)
+    total = 0
+    for k in res:
+        grandtot = res[k]
+        ngt = grandtot[:-3]
+        total += int(ngt.replace(",",""))
+    total = locale.currency(total, grouping=True)
+    # print("Grand Total: {total}".format(total=total))
+    
+    with open("output.txt", "w") as f:
+        for v in res:
+            print("{poval} = {gtval}".format(poval=v, gtval=res[v]),file=f)
+        print("", file=f)
+        print("Grand Total = {total}".format(total=total), file=f)
+            # print(v,file=f)
+    return
+
+def convert(doc, pattern):
+    text=""
+    for page_number in range(doc.page_count):
+        # Get the page object for the current page number.
+        page = doc.load_page(page_number)
+        
+        # Extract the text from the page.
+        temp = page.get_text()
+        text+=temp
+    matches = re.finditer(pattern, text, re.MULTILINE)
+    dict={}
+    for matchNum, match in enumerate(matches, start=1):
+        for groupNum in range(0, len(match.groups())):
+            groupNum = groupNum + 1
+            matchValue = match.group(groupNum)
+            labels = ["poid", "gt"]
+            if(matchValue!=None):
+                dict[labels[groupNum-1]] = matchValue
+    
+    res[dict["poid"]] = dict["gt"]
     return
 
 if __name__ == "__main__":
